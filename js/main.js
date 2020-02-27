3ed250c6900152f9a994d8933632cf3f19bed7f8
karthikeyansekar <karthikeyansep27@gmail.com>
2020-02-28 10:20:33 +1100

    TicTacToe-final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+/* Declaration of variables for each box */
 var bx1 = $('#box1');
 var bx2 = $('#box2');
 var bx3 = $('#box3');
@@ -10,12 +10,17 @@ var bx7 = $('#box7');
 var bx8 = $('#box8');
 var bx9 = $('#box9');
 
+/* Declarations to check whether the box is valid to play */
 var playValid = false;
 var win = false;
 
+/* Declarations and initialisations of variables to count player scores */
 var isPlayerOne = true;
 var isPlayerTwo = false;
+var firstPlayerScore = 0;
+var secondPlayerScore = 0;
 
+/* Definition of the function to check whether the box is valid to do the next move */
 function boxCheck(boxplayed) {
 	if ( $(boxplayed).hasClass('free') ) {
 		playValid = true;
@@ -24,6 +29,7 @@ function boxCheck(boxplayed) {
 	}
 }
 
+/* Definition of the function to reset game if any move was done by mistake and it clears the board to start from first */
 function resetGame() {
 	$('.box').removeClass('played');
 	$('.box').removeClass('U-turn');
@@ -32,20 +38,29 @@ function resetGame() {
 	$('.box').addClass('free');
 	isPlayerOne = true;
 	isPlayerTwo = false;
+
 }
 
+/* Definition of the function to calculate the score of each player and to display the winning message*/
 function winMsg(player) {
 	win = true;
 
 	if (player == "I") {
-		alert("Congratulations, you beat the player two!");
+		firstPlayerScore = firstPlayerScore + 1;
+		$('#firstPlayerScore').text(`Player 1: ${firstPlayerScore}`);
+		$('.successmsg').text('**Congratulations, Player 1 have won**');
 
-	} else {
-		alert("Player Two have Won");
+} else {
+
+		secondPlayerScore = secondPlayerScore + 1;
+		$('#secondPlayerScore').text(`Player 2: ${secondPlayerScore}`);
+	  $('.successmsg').text('**Congratulations, Player 2 have won**');
 	}
 resetGame();
 }
 
+/* Definition of the function to check whether the player have won the game and it calls other function to display the winning message
+as well */
 function conForWin() {
 
 	if ( bx1.hasClass('I-turn') && bx2.hasClass('I-turn') && bx3.hasClass('I-turn') ) {
@@ -97,6 +112,8 @@ function conForWin() {
 	}
 }
 
+/* Definition of the function to check whether the game has been drawn */
+
 function checkDraw() {
 
 	if ( !($('.box').hasClass('free')) ) {
@@ -104,6 +121,8 @@ function checkDraw() {
 		resetGame();
 	}
 }
+
+/* This function will be triggered only when the player one clicks on a box */
 
 $('.box').on('click', function Iplay() {
 	if (isPlayerOne) {
@@ -122,6 +141,8 @@ $('.box').on('click', function Iplay() {
 		alert("That box has already been played. Please choose another box");
 	}
 }
+
+/* This function executes when the player two clicks on a free box */
 else if (isPlayerTwo){
 	{
 			boxCheck(this);
@@ -142,6 +163,8 @@ else if (isPlayerTwo){
 }
 }
 });
+
+/* Declaration of the function which calls resetgame function while the button has been clicked*/
 $('#reset-button').on('click', function() {
 	resetGame();
 })
---
+++ readme.md
@@ -0,0 +1,101 @@
+# Project Name
+Tic-Tac-Toe
+
+## Table of contents
+* [LiveDemoLink](#LiveDemoLink)
+* [Description](#Description)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#Setup)
+* [CodeExamples](#CodeExamples)
+* [Features](#features)
+* [status](#status)
+* [WishList](#WishList)
+* [Contact](#contact)
+
+## LiveDemoLink
+![LiveDemoLink](https://karthikeyansekar-sydney.github.io/tictactoe/)
+
+## Description
+This is a simple game of tic tac toe that I have created with my javascript/jquery skills. Here I have created a tic-tac-toe game where two players can play against each other with a more appealing UserInterface and catchy CSS effects.
+
+## Screenshots
+![Game screenshot](./images/ScreenShot_game.png)
+
+## Technologies
+![jQuery JavaScript Library v3.4.1 ](https://jquery.com/)
+
+## Setup
+<h5>To Play the game</h5>
+* git fork(to get the code from my Repository to your remote repository)
+* git clone "URL"(to get the code from your repository to the Local Repository)
+
+<h5>To Update any codes to the existing Project do the steps as follows:</h5>
+* git add -A
+* git commit -m "your message"
+* git pull upstream master
+* git push origin master
+* Pull request to merge the changes made
+
+
+## CodeExamples
+<h5>Sample Javascript code:</h5>
+
+$('.box').on('click', function Iplay() {
+
+	if (isPlayerOne) {
+		boxCheck(this);
+		if(playValid){
+		$(this).removeClass('free');
+		$(this).addClass('played');
+		$(this).addClass('I-turn');
+		$(this).html("I");
+		isPlayerOne = false;
+		isPlayerTwo = true;
+		checkDraw();
+		conForWin();
+}
+	 else {
+		alert("That box has already been played. Please choose another box");
+	}
+}
+else if (isPlayerTwo){
+	{
+			boxCheck(this);
+
+    if(playValid){
+        $(this).removeClass('free');
+    	$(this).addClass('played');
+		$(this).addClass('U-turn');
+		$(this).html("U");
+		isPlayerOne = true;
+		isPlayerTwo = false;
+		checkDraw();
+		conForWin();
+}
+	else {
+		alert("That box has already been played. Please choose another box");
+	}
+}
+}
+});
+
+## Features
+List of features ready:
+* Two Player Game Option
+* Interactive User Interface
+* Improved look and feel
+
+To-do list:
+* Option to play against computer(AI)
+* Level of the game say easy, medium, Hard
+
+## Status
+Project is: _in progress_
+
+## WishList
+* Option to play against computer(AI)
+* Level of the game say easy, medium, Hard
+
+## Contact
+Created by [@karthik](karthikeyansep27@gmail.com) - feel free to contact me!
